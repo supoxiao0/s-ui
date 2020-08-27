33d5f691522b0452f5a740213d248f4562e46a5e
supoxiao <supoxiao@163.com>
2020-08-27 17:53:57 +0800

    init button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,47 +1,25 @@
 <template>
-<div>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
-  <p>button 的文档</p>
+<div class="button-demo">
+  <div class="size-demo">
+    <p>不同尺寸</p>
+    <div>
+      <Button size="big">大按钮</Button>
+      <Button size="medium">中等按钮</Button>
+      <Button size="small">小按钮</Button>
+    </div>
+  </div>
 </div>
 </template>
+
+<script lang="ts">
+import Button from '../libs/Button.vue'
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/libs/Button.vue
@@ -0,0 +1,14 @@
+<template>
+<button class="s-button">
+  <slot />
+</button>
+</template>
+<script lang="ts">
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  
+</style>
